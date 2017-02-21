dc21c1ad6087594084ee30244f22bb63ea2751e9
Pengkun Yang <ypengkun@gmail.com>
2017-02-20 22:53:49 -0600

    Remove two old estimators (1. require sample splitting in theory; 2. based on sinc function. ); Add Chao 1 estimator.

--- support.cpp
+++ support.cpp
@@ -89,6 +89,25 @@ double Support::estimate_J1() const
 }
 
 
+double Support::estimate_Chao1() const 
+{
+    double f1 = 0;
+    double f2 = 0;
+    for ( const auto & pair : fin )
+        if ( pair.first == 1 )
+        {
+            f1 = pair.second;
+            break;
+        }
+    for ( const auto & pair : fin )
+        if ( pair.first == 2 )
+        {
+            f2 = pair.second;
+            break;
+        }
+    return estimate_plug()+f1*(f1-1)/(2*(f2+1));
+}
+
 double Support::estimate_CL1() const // for small coefficient of variation
 {
     double N1 = estimate_TG();
@@ -120,80 +139,6 @@ double Support::estimate_CL2() const // for large coefficient of variation
 
 
 
-/* ----------------------OLD ESTIMATORS-------------------------------- */
-
-double Support::estimate_old() const
-{
-    if ( pmin < Ratio / n ) // const
-    {
-        double result = 0.0;
-        for ( const auto & pair : fin )
-            if ( pair.first < N_thr ) 
-            {
-                result += getCoeff_old(pair.first) * pair.second;
-            }
-            else
-            {
-                result += pair.second;
-            }
-        return result;
-    }
-    else
-        return estimate_plug();
-}
-
-double Support::getCoeff_old( double N ) const
-{
-    double A = ( Ratio + n*pmin ) / ( Ratio - n*pmin );
-    double a[L+1];
-	
-    ChebMore cheb(L, 1, -A); // polynomial of cos L arccos(t-A)
-    std::vector<double> a0 = cheb.expand(); // Expand: cos L arccos(x-A)=sum_i a0[i]*x^i
-    double amp = cheb.evaluate(0); // cos L arccos(-A)
-
-    // Expand: 1- cos L arccos(t-A)/cos L arccos(-A) = sum_i a[i]*x^i
-    for (int i = 0;i < L+1;i++) a[i] = - a0[i] / amp;
-    a[0] = 0;
-    
-    double s = 2 / ( Ratio - n * pmin );
-    double gL = a[L];
-    for (int i = L - 1; i>=0; i--)
-        gL = a[i] + gL * (N-i) * s;
-    return gL;
-}
-
-
-
-double Support::estimate_sinc() const
-{
-    double sum = 0.0;
-    double k = 1.0/pmin;
-    for ( const auto & pair : fin )
-    {
-        int j = pair.first;
-        int fj = pair.second;
-        if ( pair.first <= L )
-        {
-            double gj = 0;
-            double multi = -1.0;
-            for ( int i = 1; 2*i <= j; i++ )
-            {
-                multi *= - (3.1415926*k/n) * (3.1415926*k/n);
-                gj += binom( j , 2*i ) * multi / (2*i+1);
-            }
-            sum += gj * fj;
-        }
-        else
-        {
-            sum += fj;
-        }
-    }
-	
-    return sum;
-}
-
-/* ----------------------END OLD ESTIMATORS-------------------------------- */
-
 
 
 Support::Support()
@@ -205,22 +150,10 @@ Support::Support(double p_min)
     :Support()
 {
     pmin = p_min;
-    setDegree( 0.45*log(1.0/pmin) );      // Default L = 0.56 * log(k), where k = 1/pmin
-    setInterval( 0.5*log(1.0/pmin) );     // Default interval [1/k, 0.5*log(k)/n], where k = 1/pmin
+    setDegree( 0.45*log(1.0/pmin) );      // Default L = 0.45 * log(1/pmin)
+    setInterval( 0.5*log(1.0/pmin) );     // Default interval [pmin, 0.5*log(1/pmin)/n]
 }
 
-
-void Support::setFin(std::shared_ptr< const std::map<int, int> > ptr_fin_map) 
-{
-    fin.clear();
-    n = 0;
-    for ( auto it = ptr_fin_map->begin(); it != ptr_fin_map->end(); ++it )
-    {
-        int freq = it->first, cnt = it->second;
-        fin.push_back( std::make_pair( freq, cnt ) );
-        n += (freq * cnt);
-    }
-}
 
 void Support::setFin(const std::string filename) 
 {
@@ -242,7 +175,7 @@ void Support::setFin(const std::vector<int> &freq_in, const std::vector<int> &cn
 {
     fin.clear();
     n = 0;
-    for ( int i = 0; i < freq_in.size(); i++)
+    for ( unsigned i = 0; i < freq_in.size(); i++)
     {
         int freq = freq_in[i], cnt = cnt_in[i];
         fin.push_back( std::make_pair( freq, cnt ) );
--- support.h
+++ support.h
@@ -2,7 +2,6 @@
 #define SUPPORT_H
 
 #include <map>
-#include <memory>
 #include <vector>
 
 class Support
@@ -22,42 +21,33 @@ public:
     double coverage_TG() const;    // Turing-Good coverage
     double estimate_TG() const;    // Turing-Good estimator
     double estimate_J1() const;    // First order Jackknife
+    double estimate_Chao1() const;   // Chao 1
     double estimate_CL1() const;   // Chao-Lee 1
     double estimate_CL2() const;   // Chao-Lee 2
-    // Old estimators
-    double estimate_sinc() const;
-    double estimate_old() const;
-    double getCoeff_old( double N ) const;
 
 	
     void setPmin( double p_min ) { pmin = p_min; }
     void setInterval( double rEnd ){ Ratio = rEnd; }
     void setDegree( int deg ) { L = deg; }
-    void setThreshold( double N_threshold ){ N_thr = N_threshold; }
     
     // set fingerprint, also update sample size
-    void setFin( std::shared_ptr< const std::map<int, int> > ptr_fin_map ); 
     void setFin( const std::string filename );
     void setFin( const std::vector<int> &freq, const std::vector<int> &cnt );
     // set fingerprint through histogram, also update sample size
-    void setHist( const std::vector<unsigned> &hist );
     void setHist( const std::string filename );
+    void setHist( const std::vector<unsigned> &hist );
     
 
     double getPmin() const{ return pmin; }
     int getDegree() const{ return L; }
     double getInterval() const{ return Ratio; }
     int getSampleSize() const{ return n; }
-    int getThreshold() const{ return N_thr; }
-    std::vector< std::pair<int, int> > getFin() const {return fin;} // get a copy of fingerprint
 private:
-    double pmin;  // =1/k. Minimum non-zero mass
+    double pmin;  // =1/k. Preset minimum non-zero mass
     int L;        // =c0*log(k). Degree of polynomial
     double Ratio; // =c1*log(k). Approximation over [pmin,c1*log(k)/n]
     int n;        // sample size
-    
-    int N_thr;    // =c2*log(k). Used in old estimate_old. Threshold to apply polynomial estimator 
-	
+    	
     std::vector< std::pair<int, int> > fin; // Fingerprint(profile). Sorted fingerprint preferred
     
 };
